ae7dd68d2d72009e6cf7e3263ce0b33c4e4178b8
Arslan Sapashev <sapashev@gmail.com>
2016-06-22 14:37:28 +0500

    Выделил перечисление Operation в отдельный файл

---
+++ src/main/java/com/sapashev/interfaces/Operation.java
@@ -0,0 +1,11 @@
+package com.sapashev.interfaces;
+
+/**
+ * Describes type of operation (BUY, SELL)
+ * @author Arslan Sapashev
+ * @since 21.06.2016
+ * @version 1.0
+ */
+public enum Operation {
+    BUY,SELL;
+}
