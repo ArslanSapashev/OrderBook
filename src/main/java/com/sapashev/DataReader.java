5ef2ec8772556620658f72d085f7fc4392426a46
Arslan Sapashev <sapashev@gmail.com>
2016-06-22 19:21:24 +0500

    Добавил DeleteMarker

--- src/main/java/com/sapashev/DataReader.java
+++ src/main/java/com/sapashev/DataReader.java
@@ -1,6 +1,8 @@
 package com.sapashev;
 
 import com.sapashev.interfaces.Addable;
+import com.sapashev.interfaces.Operation;
+import com.sapashev.interfaces.OrderType;
 import com.sapashev.interfaces.ReadConnector;
 
 /**
@@ -12,17 +14,24 @@ import com.sapashev.interfaces.ReadConnector;
 public class DataReader implements Runnable{
     private final Addable storage;
     private final ReadConnector connector;  //source of data should know connector object
+    private DeleteMarker deleteMarker;
 
-    public DataReader(final Addable storage, final ReadConnector connector){
+    public DataReader(final Addable storage, final ReadConnector connector, DeleteMarker marker){
         this.storage = storage;
         this.connector = connector;
+        this.deleteMarker = marker;
     }
 
     public void run () {
         boolean stop = false;
         Order order;
         while (!stop){
-            if ((order = connector.read())!= null){
+            order = connector.read();
+            if(order.getType() == OrderType.DELETE){
+                deleteMarker.key = order.getOrderID();
+                deleteMarker.notify();
+            }
+            if (order != null){
                 storage.add(order);
             } else {
                 stop = true;
